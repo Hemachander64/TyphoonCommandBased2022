670f7b782b42dc833846f9857610372aa58f1228
grizzledwarvet23 <72232563+grizzledwarvet23@users.noreply.github.com>
2022-01-25 16:24:25 -0600

    Update DrivetrainWithSim.java

--- src/main/java/frc/robot/subsystems/DrivetrainWithSim.java
+++ src/main/java/frc/robot/subsystems/DrivetrainWithSim.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 public class DrivetrainWithSim extends Drivetrain //SIMS ARE GREAT!
 {
+    //LINK TO DRIVETRAIN SIM TUTORIAL: https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-simulation/drivesim-tutorial/diffdrive-sim-overview.html
+    //create the simulated versions of Drivetrain components such as encoders
+
     public DrivetrainWithSim()
     {
         
