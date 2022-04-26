820387393e80bd4da37eceb3ec37a5eeb6ee146e
Hemachander64 <83521019+Hemachander64@users.noreply.github.com>
2022-04-26 16:55:01 -0500

    We Un-nuked the Code
    Morpheus Code is now under the Morpheus Branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,7 +7,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -26,70 +25,7 @@ public final class Constants {
             public static final int RB_MOTOR_ID = 7;
         //#endregion Drivetrain IDs
             
-        //#region Drive To Distance PID Constants
-            public static final double DTD_KP = 1.15;
-            public static final double DTD_TOLERANCE = 0.1;
-            public static final double DTDP_KP = 1.5;
-            public static final double DTDP_MV = Units.feetToMeters(13.69);
-            public static final double DTDP_MA = DTDP_MV / 0.5;
-        //#endregion Drive To Distance PID Constants
-
-        //#region Turn To Angle PID Constants
-            public static final double TTAP_KP = 0.0125;
-            public static final double TTAP_MV = 1800;
-            public static final double TTAP_MA = TTAP_MV / 2;
-            public static final double TTA_TOLERANCE = 4.5;
-        //#endregion Turn To Angle PID Constants
     //#endregion Drivetrain Constants
-
-    //#region Feeder Constants
-        //#region Feeder IDs
-            public static final int FEEDER_MOTOR_ID = 15;
-        //#endregion Feeder IDs
-    //#endregion Feeder Constants
-
-    //#region Lift Constants
-        //#region Lift IDs
-            public static final int LEFT_LIFT_MOTOR_ID = 10;
-            public static final int RIGHT_LIFT_MOTOR_ID = 8;
-        //#endregion Lift IDs
-    //#endregion Lift Constants
-
-    //#region Intake Constants
-        //#region Intake IDs
-            public static final int INTAKE_MOTOR_ID = 6;
-        //#endregion Intake IDs
-    //#endregion Intake Constants
-
-    //#region Hood Constants
-        //#region Hood IDs
-            public static final int HOOD_MOTOR_ID = 410; //TODO: CHANGE THIS WHEN READY
-            public static final int HOOD_ANGLE_MOTOR_ID = 420; //TODO: change THIS!!!!!
-        //#endregion Hood IDs
-        
-        //#region Hood PID Constans
-            public static final double HOOD_KP = 0;
-        //#endregion Hood PID Constants
-    //#endregion Hood Constants
-        
-        //#region
-            public static final double kMaxSpeedMetersPerSecond = 3.0 / 3;
-            public static final double kMaxAccelerationMetersPerSecondSquared = 3.0 / 3;
-    //#region Shooter Constants
-        //#region Shooter PIDF Constants
-            public static final double SHOOT_TOLERANCE = 10;
-
-            public static final double KP_SHOOT = 0.0075;
-            
-            public static final double KS_SHOOT = 0.66;
-            public static final double KV_SHOOT = 0.00165;
-        //#endregion Shooter Constants
-
-        //#region Shooter IDs
-            public static final int LEFT_SHOOTER_MOTOR_ID = 16;
-            public static final int RIGHT_SHOOTER_MOTOR_ID = 14;
-        //#endregion Shooter IDs
-    //#endregion Shooter Constants
 
     //#region TeleOp Constants
         //#region Drive Mode Constants
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,14 +8,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+// import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.*;
-import frc.robot.commands.*;
-import frc.robot.commands.drive.*;
-import frc.robot.commands.shoot.*;
+// import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import static edu.wpi.first.wpilibj.XboxController.Button.*;
 
@@ -32,95 +30,25 @@ public class RobotContainer
 {
   // The robot's subsystems and commands are defined here...
   private final SendableChooser<Command> chooser = new SendableChooser<Command>();
-  private final Hood hood = new Hood();
-  private final Shooter shooter = new Shooter();
-  private final Lift lift = new Lift();
   private final Drivetrain dt = new Drivetrain();
-  private final Feeder feeder = new Feeder();
-  private final Intake intake = new Intake();
-  private final Limelight ll = new Limelight();
 
-  Saitek flightStick = new Saitek(0);
   XboxController driverController = new XboxController(1);
-  XboxController operatorController = new XboxController(2);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer()
   {
-    // TODO: write terminal autos
-    var rumbleOnCommand = new RunCommand(() -> operatorController.setRumble(RumbleType.kLeftRumble, 1.0)) // TODO: use this
-        .alongWith(new RunCommand(() -> operatorController.setRumble(RumbleType.kRightRumble, 1.0)));
-
-    var rumbleOffCommand = new RunCommand(() -> operatorController.setRumble(RumbleType.kLeftRumble, 0.0))
-        .alongWith(new RunCommand(() -> operatorController.setRumble(RumbleType.kRightRumble, 0.0)));
-
-    var slowButton = new JoystickButton(driverController, kLeftBumper.value)
-        .or(new JoystickButton(flightStick, Saitek.SButtons.kTrigger.ordinal()));
-    var defenseButton = new JoystickButton(driverController, kB.value)
-        .or(new JoystickButton(flightStick, Saitek.SButtons.kM.ordinal()));
-
-    var shootButton = new JoystickButton(operatorController, kA.value);
-    var lowShotButton = new JoystickButton(operatorController, kB.value);
-    var reverseShootButton = new JoystickButton(operatorController, kY.value);
-    var liftUpButton = new POVButton(driverController, 0);
-    var liftDownButton = new POVButton(driverController, 180);
-    var intakeButton = new JoystickButton(operatorController, kRightBumper.value);
-    var outtakeButton = new JoystickButton(operatorController, kLeftBumper.value);
-    var feedUpButton = new POVButton(operatorController, 0);
-    var feedDownButton = new POVButton(operatorController, 180);
-    var resetLiftEncodersButton = new JoystickButton(driverController, kStart.value);
-    var switchLiftIdleModeButton = new JoystickButton(driverController, kBack.value);
-
-    var aimButton = new JoystickButton(driverController, kRightBumper.value);
+    var slowButton = new JoystickButton(driverController, kLeftBumper.value);
+    var defenseButton = new JoystickButton(driverController, kB.value);
     
     slowButton.whenActive(new InstantCommand(dt::slowMode)).whenInactive(new InstantCommand(() -> dt.setOutput(1)));
 
     defenseButton.whenActive(new InstantCommand(dt::evilMode)).whenInactive(new InstantCommand(dt::goodMode));
 
-    shootButton.whileHeld(new ShootUpperHub(ll, shooter, hood, feeder, dt).andThen(rumbleOnCommand))
-        .whenReleased(new InstantCommand(shooter::stop, shooter).alongWith(rumbleOffCommand));    
-    lowShotButton.whileHeld(new ShootLowHub(ll, shooter, hood, feeder))
-        .whenReleased(new InstantCommand(shooter::stop, shooter));
-    reverseShootButton.whileHeld(new RunCommand(() -> shooter.setMotorVelocity(-1000), shooter))
-        .whenReleased(new InstantCommand(shooter::stop, shooter));
+    dt.setDefaultCommand(new RunCommand(() -> dt.arcadeDrive(driverController.getLeftY(), driverController.getRightX()), dt));
 
-    dt.setDefaultCommand(new RunCommand(() -> dt.arcadeDrive(-flightStick.getY() - driverController.getLeftY(),
-        flightStick.getZRotation() + driverController.getRightX()), dt));
-
-    lift.setDefaultCommand(new RunCommand(() -> lift.off(), lift));
-
-    liftUpButton.whileHeld(new RunCommand(() -> lift.on(0.5)))
-        .or(liftDownButton.whileHeld(new RunCommand(() -> lift.on(-0.5))));
-
-    intakeButton.whileHeld(new RunCommand(intake::on, intake))
-        .or(outtakeButton.whileHeld(new RunCommand(intake::reverse, intake)))
-        .whenInactive(new RunCommand(intake::off, intake));
-
-    feeder.setDefaultCommand(new RunCommand(() -> feeder.off(), feeder));
-
-    feedUpButton.whileHeld(new RunCommand(() -> feeder.on()))
-        .or(feedDownButton.whileHeld(new RunCommand(() -> feeder.reverse())))
-        .whenInactive(new RunCommand(feeder::off, feeder));
-
-    aimButton.whileHeld(new AimCommand(dt, ll::getTx));
-    // resetLiftEncodersButton.whenPressed(new InstantCommand(lift::resetEncoders, lift));
-    // switchLiftIdleModeButton.whenHeld(new RunCommand(lift::coastMode, lift))
-        // .whenReleased(new InstantCommand(lift::brakeMode, lift));
-
-    chooser.setDefaultOption("TopBallAuto", new TopBallAuto(ll, shooter, hood, feeder, intake, dt));
-    chooser.addOption("MiddleBallAuto", new MiddleBallAuto(ll, shooter, hood, feeder, intake, dt));
-    chooser.addOption("LowBallAuto", new LowBallAuto(ll, shooter, hood, feeder, intake, dt));
-    chooser.addOption("No auto", new ShootLowHub(ll, shooter, hood, feeder).andThen(new RunCommand(feeder::on, feeder).withTimeout(2)));
-    chooser.addOption("No-ish auto", new ShootLowHub(ll, shooter, hood, feeder).andThen(new RunCommand(feeder::on, feeder).withTimeout(2)).andThen(new DTDProfiled(1, dt)));
-    chooser.addOption("Drive To Distance", new DriveToDistanceCommand(1, dt));
-    chooser.addOption("Turn To Angle", new TurnToAngleCommand(90, dt));
-    chooser.addOption("DTDProfiled", new DTDProfiled(1, dt));
-    chooser.addOption("TTAProfiled", new TTAProfiled(90, dt));
-    chooser.addOption("SquarePath", new SquareAuto(dt));
-    chooser.addOption("Ramsete Trajectory Command", new RamseteTrajCommand(dt));
-    chooser.addOption("SetWheelSpeeds", new RunCommand(() -> dt.setWheelSpeeds(1, 1), dt));
+    // chooser.addOption("SetWheelSpeeds", new RunCommand(() -> dt.setWheelSpeeds(1, 1), dt));
 
     SmartDashboard.putData(chooser);
   }
@@ -137,7 +65,6 @@ public class RobotContainer
 
   public void disabledInit()
   {
-    shooter.stop();
     dt.resetEncoders();
     // dt.resetOdometry(new Pose2d()); // we aren't using odometry at waco
   }
